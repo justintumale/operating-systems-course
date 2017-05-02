9f04b0b169140f0a6badcbf40182ba2137f39497
justintumale <fjetumale@yahoo.com>
2017-05-02 00:52:57 -0700

    implemented writers

--- readersWriters/main.c
+++ readersWriters/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 #define NUM_THREADS 10
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -13,18 +14,18 @@ pthread_cond_t no_readers;
 pthread_cond_t no_writers;
 
 void *reader(int* id) {
-
+    //usleep(1000 * (random() % 10));
     //acquire mutex. Wait if condition is not satisfied
     pthread_mutex_lock(&mutex);
         while (write_count > 0) {
             pthread_cond_wait(&no_writers, &mutex);
         }
-        printf("\nstarting %d \n", *id);
+        printf("\nstarting reader thread %d \n", *id);
         read_count++;
     pthread_mutex_unlock(&mutex);
 
     //perform READ
-    printf("performing read:\n");
+    printf("performing read...\n");
     FILE *fp;
     //fp = fopen(path, "r");
     fp = fopen("<filepath>", "r");
@@ -48,7 +49,8 @@ void *reader(int* id) {
     pthread_mutex_unlock(&mutex);
 }
 
-void *writer(void *arg) {
+void *writer(int *id) {
+    //usleep(1000 * (random() % 10));
     //acquire mutex. Wait if condition is not satisfied.
     pthread_mutex_lock(&mutex);
         while (read_count > 0) {
@@ -57,13 +59,23 @@ void *writer(void *arg) {
                 pthread_cond_wait(&no_writers, &mutex);
             }
         }
+        printf("\nstarting writer thread %d \n", *id);
         write_count++;
     pthread_mutex_unlock(&mutex);
 
     //perform WRITE
+    printf("performing write...\n");
+    FILE *fp;
+    //fp = fopen(path, "r");
+    fp = fopen("<filepath>", "a");
+    fprintf(fp, "\nwrite from thread %d", *id);
+    fflush(fp);
+    fclose(fp);
+    printf("File closed.\n");
 
     //reacquire mutex. Update conditional variable and boadcast readers and signal writers.
     pthread_mutex_lock(&mutex);
+        printf("write performed. \n");
         write_count--;
         if  (write_count == 0) {
             pthread_cond_broadcast(&no_writers);
@@ -76,14 +88,16 @@ int main() {
     int i;
     pthread_t tid[NUM_THREADS];
     int threadId[NUM_THREADS];
+    srandom((unsigned int)time(NULL));
 
     for (i = 0; i < NUM_THREADS; i++) {
         threadId[i] = i;
 
-        if (i % 2 == 0) {
-            printf("\n creating thread %d", threadId[i]);
+        if (i < 5) {
+            printf("\n creating reader thread %d", threadId[i]);
             pthread_create(&tid[i], NULL, reader, &threadId[i]);
         } else {
+            printf("\n creating writer thread %d", threadId[i]);
             pthread_create(&tid[i], NULL, writer, &threadId[i]);
         }
     }
