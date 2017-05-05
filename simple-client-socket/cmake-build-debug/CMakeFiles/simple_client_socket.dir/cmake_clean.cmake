f4fa8012690213104cf6e2d906bbe2314ee3bddc
justintumale <fjetumale@yahoo.com>
2017-05-04 22:29:43 -0700

    initial commit for simple client socket

---
+++ simple-client-socket/cmake-build-debug/CMakeFiles/simple_client_socket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_client_socket.dir/main.c.o"
+  "simple_client_socket.pdb"
+  "simple_client_socket"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_client_socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
