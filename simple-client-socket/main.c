5fe09163c55582a6c9c2e0afee2a57bb8d940c51
justintumale <fjetumale@yahoo.com>
2017-05-04 23:20:35 -0700

    started implementing connection to remote server

--- simple-client-socket/main.c
+++ simple-client-socket/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<sys/socket.h>
+#include<arpa/inet.h>
 
 /*
  * Write a simple C program that creates, initializes, and connects a client socket to a server socket.
@@ -10,6 +11,7 @@
 int main() {
     //Create the client socket
     int socket_desc;
+    printf("creating socket...");
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
         //Address Family -AF_INET(this is IP version 4)
         //Type - SOCK_STREAM (this means connection oriented TCP protocol)
@@ -17,5 +19,25 @@ int main() {
     if (socket_desc == -1) {
         printf("Could not create socket.");
     }
+    printf("socket created");
+
+    //Connect to a server
+        //need ip address and port number to connect to
+
+        //1. Create a sockaddr_in structure
+    struct sockaddr_in server;
+    server.sin_addr.s_addr = inet_addr("74.125.235.20");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+
+        //2. Connect to remote server
+    printf("Connecting...");
+    if (connect(socket_desc, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        printf("connect error");
+        return 1;
+    }
+
+    printf("Connected");
+
     return 0;
 }
