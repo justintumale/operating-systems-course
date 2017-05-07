02eb9df5611656a0ecc70fcccafb8f6ac2d10881
justintumale <fjetumale@yahoo.com>
2017-05-06 20:31:17 -0700

    initialized server socket code

---
+++ simple-server-socket/cmake-build-debug/CMakeFiles/simple_server_socket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_server_socket.dir/main.c.o"
+  "simple_server_socket.pdb"
+  "simple_server_socket"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_server_socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ simple-server-socket/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Write a simple C program that creates and initializes a server socket. Once initialized, the server should accept a
+ * client connection, close the connection, and then exit. You should provide a way to specify the server's listening
+ * port. This can be hardcoded or passed via the command line.
+ */
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
