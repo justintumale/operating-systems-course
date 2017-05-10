7968a71461cd6f640a71e7df1cc077c8ffd50f87
justintumale <fjetumale@yahoo.com>
2017-05-10 01:20:38 -0700

    first draft of client socket

--- echo-protocol/client.c
+++ echo-protocol/client.c
@@ -5,8 +5,29 @@
  */
 
 #include <stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
 
 int main() {
-    //Create client socket that specifies server hostname and port number
+    //Create client socket
+    int client_socket_fd = socket(AF_INET, SOCK_STREAM, 0)
+    if (client_socket_fd < 0) {
+        perror("Error creating client socket");
+        return -1;
+    }
+
+    //Initialize socketaddr_in data structure
+    struct sockaddr_in server_socket;
+    server_socket.sin_family = AF_INET;
+    server_socket.sin_port = htons(80);
+    server_socket.sin_addr.s_addr = inet("XX.XX.XX.XX");
+
+    int connect = connect(client_socket_fd, (struct sockaddr*) &server_socket, sizeof(server_socket));
+    if (connect < 0) {
+        perror("Error connecting to server socket");
+        return -1;
+    }
+
+
     return 0;
 }
